20682c7c93b3e41567e64c5effcdc2f6812ad85f
Vlad-Ber <vladik@live.se>
2020-11-22 00:05:39 +0100

    Update Assignment.java

--- CalcPars/Assignment.java
+++ CalcPars/Assignment.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 /**
  * @file Assignment.java
- * @author Erik Norén, Andreas Bleichner
+ * @author Erik Norén, Vladislav Bertilsson
  * @date 25 Nov 2019
  * @brief Assignment is a class that handles Binary-class expressions that are mathematical assignments
  *
