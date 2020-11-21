258623b3647038c540c61ebd19d8889a848d6125
Vlad-Ber <vladik@live.se>
2020-11-22 00:07:40 +0100

    Update Exp.java

--- CalcPars/Exp.java
+++ CalcPars/Exp.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 /**
  * @file Exp.java
- * @author Erik Norén, Andreas Bleichner
+ * @author Erik Norén, Vladislav Bertilsson
  * @date 25 Nov 2019
  * @brief Exp is a class that handles Unary-class expressions that are mathematical exponential expressions
  *
