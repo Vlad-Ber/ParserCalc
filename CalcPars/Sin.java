6886943ede66064495bc36732163ca082d9585bc
Vlad-Ber <vladik@live.se>
2020-11-22 00:09:07 +0100

    Update Sin.java

--- CalcPars/Sin.java
+++ CalcPars/Sin.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 /**
  * @file Sin.java
- * @author Erik Norén, Andreas Bleichner
+ * @author Erik Norén, Vladislav Bertilsson
  * @date 25 Nov 2019
  * @brief Sin is a class that handles Unary-class expressions that are mathematical sine expressions
  *
