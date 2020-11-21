db08d1f552cb745334b2af47afba12bc16bd3c01
Vlad-Ber <vladik@live.se>
2020-11-22 00:08:35 +0100

    Update Multiplication.java

--- CalcPars/Multiplication.java
+++ CalcPars/Multiplication.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 /**
  * @file Multiplication.java
- * @author Erik Norén, Andreas Bleichner
+ * @author Erik Norén,Vladislav Bertilsson
  * @date 25 Nov 2019
  * @brief Multiplication is a class that handles Binary-class expressions that are mathematical multiplications
  *
