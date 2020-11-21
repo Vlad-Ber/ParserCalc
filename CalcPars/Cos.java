26d091f241313ae90dae6f02bd6f44f1066acf22
Vlad-Ber <vladik@live.se>
2020-11-22 00:07:10 +0100

    Update Cos.java

--- CalcPars/Cos.java
+++ CalcPars/Cos.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 /**
  * @file Calculator.java
- * @author Erik Norén, Andreas Bleichner
+ * @author Erik Norén, Vladislav Bertilsson
  * @date 25 Nov 2019
  * @brief Cos is a class that handles Unary-class expressions that are mathematical cosine expressions
  *
